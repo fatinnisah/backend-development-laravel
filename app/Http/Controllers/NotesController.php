13250cfd8453f5e11a86e3415730e7d2d7c62151
fatinnisah <fatinnurhanisah@gmail.com>
2026-02-11 12:22:48 +0800

    any changes has been made

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,51 +2,77 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Note;
+use Illuminate\Http\Request;
+
 
 class NotesController extends Controller
 {
-    // get all, store, update, destroy
+   // get all, store, update, destroy
     public function index()
     {
-        // tarik data dari 'notes' table
-        // guna Note model
-        // guna method all()
-        // wakilkan guna variable notes
-
+    //tarik data daripada 'notes' table
+    //guna Note model
+    //guna method all ()
+    //wakilkan guna variable $notes
+    //return as json response
         $notes = Note::all();
-        return $notes;
-
+        return response()->json($notes);
     }
-    //how does this 'store method get the Request
-    public function store (Request $request){
-        // get the request via Request class
-        // get the input: title, content
-        // validate the data, datatype is correct
-        // make the Note model saves the title and content in the 'notes' table
-        // return json response saying that the store operation is successful
-        $validated = $request->validate([
+
+    //how does this 'store' method work?
+    public function store(Request $request)
+    {
+    //get the request via Request class
+    //get the input: title, content
+    //validate the data, datatype is correct
+    //make the Note model saves the title and contect in the 'notes' table
+    //return json response saying that the store operation is successsful
+    $validated = $request->validate([
             'title' => 'string',
             'content' => 'string',
         ]);
-        // saving/storing the title and content into the database
-        $note = Note::create($validated);
+    //saving/storing the title and content into the database
         
+        $note = Note::create($validated);
         return response()->json([
-            'success' => true,
-            'data' => $note
-     ]);
-     
-     // json response that we will get
-     // {
-     //   success:true,
-     //   data: {
-     //        title: "Buku Laravel",
-     //        content: "Nanti beli buku Laravel di kedai buku"
-     //    }
-     // }
-    }
+        'success'=>true,
+        'data'=>$note
+        ], 201);
     
+
+    //json response that we will get
+    // {
+    // "success": true,
+    // "data": { ... }}
+    public function update(Request $request, $id)
+    {
+        $note = Note::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'title' => 'string',
+            'content' => 'string',
+        ]);
+
+        $note -> update($validated);
+
+        return response()->json([
+        'success'=>true,
+        'data'=>$note
+        ], 200);
+
+    }
+
+    public function destroy($id)
+    {
+        $note = Note::find($id);
+
+        if (!$note) {
+            return response()->json(['message' => 'Note not found'], 404);
+        }
+
+        $note->delete();
+
+        return response()->json(['message' => 'Note deleted']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotesController;
 
-Route::get('notes', [NotesController::class, 'index']);
+Route::get ('notes', [NotesController::class, 'index']);
 Route::post('notes', [NotesController::class, 'store']);
 Route::put('notes/{id}', [NotesController::class, 'update']);
 Route::delete('notes/{id}', [NotesController::class, 'destroy']);
