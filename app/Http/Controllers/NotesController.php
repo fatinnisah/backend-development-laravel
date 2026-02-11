b7c16658c05b3527f34d9bda90052d7f9673f781
fatinnisah <fatinnurhanisah@gmail.com>
2026-02-11 16:34:55 +0800

    modified:   app/Http/Controllers/NotesController.php

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,77 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Note;
 use Illuminate\Http\Request;
-
+use App\Models\Note;
 
 class NotesController extends Controller
 {
-   // get all, store, update, destroy
     public function index()
     {
-    //tarik data daripada 'notes' table
-    //guna Note model
-    //guna method all ()
-    //wakilkan guna variable $notes
-    //return as json response
-        $notes = Note::all();
-        return response()->json($notes);
+        return Note::all();
     }
 
-    //how does this 'store' method work?
     public function store(Request $request)
     {
-    //get the request via Request class
-    //get the input: title, content
-    //validate the data, datatype is correct
-    //make the Note model saves the title and contect in the 'notes' table
-    //return json response saying that the store operation is successsful
-    $validated = $request->validate([
-            'title' => 'string',
-            'content' => 'string',
-        ]);
-    //saving/storing the title and content into the database
-        
-        $note = Note::create($validated);
-        return response()->json([
-        'success'=>true,
-        'data'=>$note
-        ], 201);
-    
+        $note = Note::create($request->all());
+        return response()->json($note, 201);
+    }
 
-    //json response that we will get
-    // {
-    // "success": true,
-    // "data": { ... }}
     public function update(Request $request, $id)
     {
         $note = Note::findOrFail($id);
 
-        $validatedData = $request->validate([
+        $validated = $request->validate([
             'title' => 'string',
             'content' => 'string',
         ]);
 
-        $note -> update($validated);
+        $note->update($validated);
 
-        return response()->json([
-        'success'=>true,
-        'data'=>$note
-        ], 200);
-
+        return response()->json($note);
     }
 
     public function destroy($id)
     {
-        $note = Note::find($id);
-
-        if (!$note) {
-            return response()->json(['message' => 'Note not found'], 404);
-        }
-
+        $note = Note::findOrFail($id);
         $note->delete();
 
-        return response()->json(['message' => 'Note deleted']);
+        return response()->json(['message' => 'Deleted successfully']);
     }
 }
